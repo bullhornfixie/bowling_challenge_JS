02e7247da1ad536e8df0c5c60bcfabc91c4819a0
bullhornfixie <dawson.jon@hotmail.co.com>
2021-02-16 11:31:36 +0000

    new project

---
+++ bowling-challenge-js/src/bowling.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Game {
+
+  constructor(){
+    this.frameCount = 0
+    this.totalScore = 0
+    this.spareBonus = false
+  };
+
+  start() {
+    this.frameCount ++;
+  }
+
+}
+
