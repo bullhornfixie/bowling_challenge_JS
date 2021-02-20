dc64118d7e37f6869186d6e7738c5fa74d195c42
bullhornfixie <dawson.jon@hotmail.co.com>
2021-02-20 15:07:46 +0000

    Jquery responding to game.js and tested a single roll on web app

--- bowling-challenge-js/jquery_interface.js
+++ bowling-challenge-js/jquery_interface.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
   let kingpin = new Game();
 
-$('#restart').click(function() {
-  console.log("game restarted")
-})
-
 $('#pin-1').click(function() {
-  kingpin.roll(1)
-  $('#frame-1').text(kingpin.score[1]);
-  $('#frame-2').text(kingpin.score[2]);
+  kingpin.receiveShot(1)
+  $('#frame-1').text(kingpin.frameScore);
 })
-
 
 });
 
--- bowling-challenge-js/src/game.js
+++ bowling-challenge-js/src/game.js
@@ -12,9 +12,19 @@ class Game {
 Game.prototype.receiveShot = function(hitpins) {
   this.shotCount++;
   this.pinCount -= hitpins;
-  this.frameScore += hitpins;
+  this.frameScore += hitpins; 
+  // if(this.shotCount === 1 && this.bonus = "/") { hitpins * 2 }
   if (this.shotCount === 2 || this.pinCount === 0 ) { return "end frame"}
+  // once above condition is true need to execute below functions 
 };
+
+Game.prototype.strikeOrSpare = function() {
+  let bonus = ""
+  if(this.pinCount === 0 && this.shotCount === 1 ) { bonus = "X" }
+  else if(this.pinCount === 0 && this.shotCount === 2 ) { bonus = "/" }
+  return bonus 
+  // need to configure how bonus score will be implemented for a spare /
+}
 
 Game.prototype.updateTotalScore = function() {
   this.scoreCard.push(this.frameScore)
@@ -24,13 +34,6 @@ Game.prototype.displayTotalScore = function() {
   return this.scoreCard.reduce((a, b) => a + b, 0);
 }
 
-Game.prototype.strikeOrSpare = function() {
-  let bonus = ""
-  if(this.pinCount === 0 && this.shotCount === 1 ) { bonus = "X" }
-  else if(this.pinCount === 0 && this.shotCount === 2 ) { bonus = "/" }
-  return bonus 
-}
-
 Game.prototype.frameReset = function() {
   this.pinCount = 10; 
   this.shotCount = 0;
@@ -38,9 +41,3 @@ Game.prototype.frameReset = function() {
 }
 
 
-/* when frame ends
-  update frameScore [] with score 
-  display frameScore
-  track spares or strikes for frame 
-  reset frame data at end of frame
-*/
